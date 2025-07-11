c816482803b5c9e230f5c48c4408bf32b8896942
TP211 <1242410592@qq.com>
2025-07-11 18:20:00 +0800

    Added Base Components

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+import tkinter
+import socket
+import urllib
+import json
+import os
+import sys
+import subprocess
+import threading
+import time
+import logging
+
+def main():
+    print("Unavailable for now!")
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyMCL',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='Apache 2.0',
+    author='TP211',
+    author_email='TP211@foxmail.com',
+    description='Python Minecraft Launcher'
+)
